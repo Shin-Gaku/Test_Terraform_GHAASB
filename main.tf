e2b11d1b10a292a33ddaad7e60d0259110b5a243
Shin Gaku <yakushin-co.ltd@gol.com>
2025-11-28 22:11:35 +0900

    コンソールで手動作成につき、oidc-role作成コードを削除

--- main.tf
+++ main.tf
@@ -56,57 +56,6 @@ resource "aws_s3_bucket_policy" "policy_tfstate" {
   })
 }
 
-#============================================================================
-# Github Actions 用ロール
-#============================================================================
-# -----------------------------------------------------------------------------
-# GitHub Actions プロバイダー設定
-# -----------------------------------------------------------------------------
-resource "aws_iam_openid_connect_provider" "terraform_cicd" {
-  url            = "https://token.actions.githubusercontent.com"
-  client_id_list = ["sts.amazonaws.com"]
-  # このコードは固定値
-  # OIDC ID プロバイダーのサムプリント
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
-}
-
-# -----------------------------------------------------------------------------
-# GitHub Actions 用ロール作成
-# -----------------------------------------------------------------------------
-resource "aws_iam_role" "terraform_cicd_oidc_role" {
-  name = "TerraCICDDemoOIDCRole"
-  path = "/"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Action = "sts:AssumeRoleWithWebIdentity"
-      Principal = {
-        Federated = aws_iam_openid_connect_provider.terraform_cicd.arn
-      }
-      Condition = {
-        StringLike = {
-          "token.actions.githubusercontent.com:sub" = [
-            # リポジトリ制限
-            # xxxxx：GitHubのアカウント名
-            "repo:xxxxx/terraform_test:*",
-            "repo:Shin-Gaku/Test_Terraform_GHAASB:*",
-          ]
-        }
-      }
-    }]
-  })
-}
-
-# -----------------------------------------------------------------------------
-# ポリシーのアタッチ（AdministratorAccess_attachment）
-# -----------------------------------------------------------------------------
-resource "aws_iam_role_policy_attachment" "AdministratorAccess_attachment" {
-  role = aws_iam_role.terraform_cicd_oidc_role.name
-  # Admin権限を指定
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
-
 # ---------------------------------------------
 # VPC (モジュールから呼び出す)
 # ---------------------------------------------
