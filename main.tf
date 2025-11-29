79722b37ab388ef157389549ea050c6fb8cdfe00
Shin Gaku <yakushin-co.ltd@gol.com>
2025-11-29 22:49:44 +0900

    ansibleのデバッグ用にvpcとec2のみ実行するように

--- main.tf
+++ main.tf
@@ -76,65 +76,6 @@ module "ec2Instance" {
   ssh_fixed_ip  = var.ssh_fixed_ip
   subnet_id     = module.aws-study-vpc.public_subnets_ids[0]
   vpc_id        = module.aws-study-vpc.vpc_id
-  alb_sg_id     = module.alb.alb_sg_id
+  #alb_sg_id     = module.alb.alb_sg_id
   modules_name  = "aws-study"
-}
-
-# ---------------------------------------------
-# ALB (モジュールから呼び出す)
-# ---------------------------------------------
-module "alb" {
-  source       = "./modules/alb"
-  ec2_id       = module.ec2Instance.instance_id
-  subnet_ids   = module.aws-study-vpc.public_subnets_ids
-  vpc_id       = module.aws-study-vpc.vpc_id
-  modules_name = "aws-study"
-}
-
-# ------------------------------------------------------
-# Secrets Managerからdb_password取得
-# ------------------------------------------------------
-data "aws_secretsmanager_secret" "rds_password" {
-  name = "${var.name_environment}/rds/mysql/password"
-}
-
-data "aws_secretsmanager_secret_version" "rds_secret_version" {
-  secret_id = data.aws_secretsmanager_secret.rds_password.id
-}
-
-locals {
-  credentials = jsondecode(data.aws_secretsmanager_secret_version.rds_secret_version.secret_string)
-}
-
-# ---------------------------------------------
-# RDSインスタンス (モジュールから呼び出す)
-# ---------------------------------------------
-module "rds" {
-  source             = "./modules/rds"
-  vpc_id             = module.aws-study-vpc.vpc_id
-  private_subnet_ids = module.aws-study-vpc.private_subnets_ids
-  ec2_sg_id          = module.ec2Instance.ec2_sg_id
-  rds_username       = var.db_username
-  rds_password       = local.credentials["password"]
-  AZs                = ["ap-northeast-1a", "ap-northeast-1c"]
-  modules_name       = "aws-study"
-}
-
-# ---------------------------------------------
-#  CloudWatch Alarm (モジュールから呼び出す)
-# ---------------------------------------------
-module "cloudwatchAlarm" {
-  source             = "./modules/cloudwatch"
-  ec2_id             = module.ec2Instance.instance_id
-  notification_email = var.notification_email
-  modules_name       = "aws-study"
-}
-
-# ---------------------------------------------
-# WAF WebACL (モジュールから呼び出す)
-# ---------------------------------------------
-module "waf" {
-  source       = "./modules/wafacls"
-  alb_arn      = module.alb.alb_arn
-  modules_name = "aws-study"
 }
